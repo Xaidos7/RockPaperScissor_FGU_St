c795b51708982ed796daf57228ec526224277bc1
Pmathie <cykelrytteren@hotmail.com>
2026-03-13 10:48:56 +0100

    Added win conditions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,12 +18,34 @@ public class GameManager : MonoBehaviour
     public void ButtonPress(GameObject playerChoice)
     {
         GameObject enemyChoice = enemyChoices[Random.Range(0, enemyChoices.Length)];
+        foreach (GameObject a in playerChoices)
+        {
+            a.SetActive(false);
+        }
+        foreach (GameObject b in enemyChoices)
+        {
+            b.SetActive(false);
+        }
+        
+        enemyChoice.SetActive(true);
+        playerChoice.SetActive(true);
+
         Debug.Log("Du valgte: " + playerChoice.tag);
         Debug.Log("Din modstander valgte: " + enemyChoice.tag);
 
         if (playerChoice.CompareTag(enemyChoice.tag))
         {
             Debug.Log("Den er uafgjort!");
+        }
+        else if (playerChoice.CompareTag("Sten") && enemyChoice.CompareTag("Saks")|| 
+                 playerChoice.CompareTag("Saks") && enemyChoice.CompareTag("Papir")||
+                 playerChoice.CompareTag("Papir") && enemyChoice.CompareTag("Sten"))
+        {
+            Debug.Log("Du vandt!");
+        }
+        else
+        {
+            Debug.Log("Du tabte!");
         }
 
     }
